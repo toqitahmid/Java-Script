80fa2deac835e293932786854498f596b69bd1b4
toqitahmid <rubaiathali@gmail.com>
2026-06-02 17:28:50 +0600

    gain knowledge about how works copy and reference

---
+++ Copy Vs Referance/copy.js
@@ -0,0 +1,13 @@
+let a = 20;
+let b = a;
+a = 10;
+console.log(`a = `, a);
+console.log(`b = `, b);
+
+let myName = 'Araf';
+let friendName = myName;
+
+myName = 'Toqi';
+
+console.log(`My name : `, myName);
+console.log(`Friend's name: `, friendName);
---
+++ Copy Vs Referance/referance.js
@@ -0,0 +1,5 @@
+let a = [10,20,30];
+let b = a;
+b.pop();
+console.log(a);
+console.log(b);
