fae2f9454fa5c59df842120068878b64a9f287d5
toqitahmid <rubaiathali@gmail.com>
2026-06-12 19:51:22 +0600

    do type coecrion in js

---
+++ Type Coercion/script.js
@@ -0,0 +1,9 @@
+let x;
+x = "5" + 1;       //number converted to string
+console.log(x);
+x = "5" - 1;       //string converted to number
+console.log(x);
+x = true + 1;      //2
+console.log(x)
+x = undefined + 1; //NaN
+console.log(x)
