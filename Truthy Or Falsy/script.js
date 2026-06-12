4a06450a9d615708aede7269b82711ac0a6b369c
toqitahmid <rubaiathali@gmail.com>
2026-06-12 22:26:04 +0600

    console log truthy and falsy value

---
+++ Truthy Or Falsy/script.js
@@ -0,0 +1,16 @@
+// Falsy Values
+console.log("False: "+!!false);
+console.log("0: "+!!0);
+console.log("Empty String: "+!!"");
+console.log("Null: "+!!null);
+console.log("Undefined: "+!!undefined);
+console.log("NaN: "+!!NaN);
+
+console.log();
+console.log();
+
+//Truthy Values
+console.log("String: "+!!"Araf");
+console.log("Aray: "+!![]);
+console.log("Object: "+!!{});
+console.log("Function: "+!!function(){});
