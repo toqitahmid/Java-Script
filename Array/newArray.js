a91aafd160629f4e37233cf3ab270fd41ecccc9a
toqitahmid <rubaiathali@gmail.com>
2026-06-13 23:03:43 +0600

    learn new array

---
+++ Array/newArray.js
@@ -0,0 +1,9 @@
+//create an array with one element;
+let points = [10]; 
+console.log(points);
+
+//create an array with 10 undefined elements;
+points = new Array(10);
+console.log(points);
+points[0] = 1;
+console.log(points);
