b803b9e7003d5861e2297f754c668abae7fe1d6a
toqitahmid <rubaiathali@gmail.com>
2026-06-13 23:04:53 +0600

    learn associative array in js

---
+++ Associative Array/script.js
@@ -0,0 +1,17 @@
+const person = [];
+person [0] = 'Toqi';
+person[1] = 'Tahmid';
+person[2] = 21;
+
+console.log(person.length);
+
+// in js arrays always use numbered indexes. so it can return it's lenght;
+
+const student = [];
+student['firstName'] = 'Toqi';
+student['lastName'] = 'Tahmid';
+student['age'] = 21;
+
+console.log(student.length);
+
+//js does not support array with named inexes so it can't return it's length;
