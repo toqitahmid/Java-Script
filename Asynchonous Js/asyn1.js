098523d1b921df36307cdb8346861cf6c11eac16
toqitahmid <rubaiathali@gmail.com>
2026-06-16 00:06:34 +0600

    learn asynchoronous js

---
+++ Asynchonous Js/asyn1.js
@@ -0,0 +1,7 @@
+console.log('line 1');
+
+setTimeout(function(){
+    console.log('line 2');
+},0);
+
+console.log('line 3');
---
+++ Asynchonous Js/asyn2.js
@@ -0,0 +1,35 @@
+const paymentSuccess = true;
+const mark = 70;
+
+function enroll(callback){
+    console.log('Course enrolled is in progress');
+
+    setTimeout(function(){
+        if(paymentSuccess){
+            callback();
+        }
+        else{
+            console.log("Payment failed!");
+        }
+    },2000)
+}
+
+function progress(callback){
+    console.log('Course on progress...');
+    setTimeout(function(){
+        if(mark >= 80){
+            callback();
+        }
+        else{
+            console.log('You could not get enough marks to get the certificate')
+        }
+    },3000)
+}
+
+function getCertificate(){
+    console.log('You got a Certificate!')
+}
+
+enroll(function(){
+    progress(getCertificate);
+});
