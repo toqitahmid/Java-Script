4785e2db826fe390e317abf106b53d0637bca95b
toqitahmid <rubaiathali@gmail.com>
2026-06-23 22:23:27 +0600

    recap sort method

--- Array Method/Sort/script.js
+++ Array Method/Sort/script.js
@@ -1,15 +1,18 @@
 let numbers = [20,50,10,30,60,40,100,70,90,80]; 
 
+
 // assending sort:
 numbers.sort(function(a,b){
     return a - b;
 })
 console.log(numbers);
 
+
 //dessending sort:
 numbers.sort(function(a,b){
     return b - a;
 })
 console.log(numbers);
+
 
 console.log(Math.max.apply(null,numbers))
